938768ff6423b2046bfe029de53880e18abe5013
Felix Martinez <felixaviermartinez@gmail.com>
2019-07-29 17:30:10 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -7,3 +7,7 @@ second_number = 5
 sum = first_number + second_number
 
 difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
